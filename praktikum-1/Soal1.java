1effb89cc8212373daf9b1ad8e743b27e6341149
ufarqrobbany <faruqrobbany@gmail.com>
2024-02-15 07:32:20 +0700

    praktikum 1

---
+++ praktikum-1/Soal1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Umar Faruq Robbany
+ */
+public class Soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angka1 + angka2);
+        
+        System.out.println("Hasil 1 " + hasil);
+    }
+}
---
+++ praktikum-1/Soal2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Umar Faruq Robbany
+ */
+public class Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        int i = 42;
+        String s = (i < 40) ? "life" : (i > 50) ? "universe" : "everything";
+        System.out.println(s);
+    }
+}
