af236fc1e9f749acc3263619857bf53bb89ff718
ufarqrobbany <faruqrobbany@gmail.com>
2024-02-29 10:57:07 +0700

    add circle

--- praktikum/pertemuan_4/exercise2/src/task2_1/Circle.java
+++ praktikum/pertemuan_4/exercise2/src/task2_1/Circle.java
@@ -29,4 +29,18 @@ public class Circle extends Shape {
     this.radius = radius;
   }
 
+  // method
+  public double getArea() {
+    return radius * radius * Math.PI;
+  }
+
+  public double getPerimeter() {
+    return 2 * Math.PI * radius;
+  }
+
+  @Override
+  public String toString() {
+    return "A Circle with radius=" + radius + ", which is a subclass of " + super.toString();
+  }
+
 }
