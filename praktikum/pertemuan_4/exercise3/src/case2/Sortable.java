417d84b832b00810229f974237f27031d9c90abc
ufarqrobbany <faruqrobbany@gmail.com>
2024-03-03 18:40:10 +0700

    create case 2

---
+++ praktikum/pertemuan_4/exercise3/src/case2/Sortable.java
@@ -0,0 +1,21 @@
+package case2;
+
+abstract class Sortable {
+  public abstract int compare(Sortable b);
+
+  public static void shell_sort(Sortable[] a) {
+    // Shell sort body
+    int n = a.length;
+
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+      for (int i = gap; i < n; i++) {
+        Sortable temp = a[i];
+        int j;
+        for (j = i; j >= gap && a[j - gap].compare(temp) > 0; j -= gap) {
+          a[j] = a[j - gap];
+        }
+        a[j] = temp;
+      }
+    }
+  }
+}
