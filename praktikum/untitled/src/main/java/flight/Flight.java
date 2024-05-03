8109c5f64878ad994e168b4e88546d7224c6a758
ufarqrobbany <faruqrobbany@gmail.com>
2024-05-03 09:08:48 +0700

    update 11

---
+++ praktikum/untitled/src/main/java/flight/Flight.java
@@ -0,0 +1,51 @@
+package flight;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Flight {
+    private String id;
+    private List<Passenger> passengers = new ArrayList<Passenger>();
+    private String flightType;
+
+    public Flight(String id, String flightType) {
+        this.id = id;
+        this.flightType = flightType;
+    }
+    public String getId() {
+        return id;
+    }
+    public List<Passenger> getPassengersList() {
+        return Collections.unmodifiableList(passengers);
+    }
+    public String getFlightType() {
+        return flightType;
+    }
+    public boolean addPassenger(Passenger passenger) {
+        switch (flightType) {
+            case "Economy":
+                return passengers.add(passenger);
+            case "Business":
+                if (passenger.isVip()) {
+                    return passengers.add(passenger);
+                }
+                return false;
+            default:
+                throw new RuntimeException("Unknown type: " + flightType);
+        }
+    }
+    public boolean removePassenger(Passenger passenger) {
+        switch (flightType) {
+            case "Economy":
+                if (!passenger.isVip()) {
+                    return passengers.remove(passenger);
+                }
+                return false;
+            case "Business":
+                return false;
+            default:
+                throw new RuntimeException("Unknown type: " + flightType);
+        }
+    }
+}
